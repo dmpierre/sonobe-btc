bbb99bb1440de7db1ce3c2bc047a3233a2050c62
dmpierre <pdaixmoreux@gmail.com>
2024-05-03 15:22:35 +0200

    feat: full flow working

--- src/main.rs
+++ src/main.rs
@@ -6,83 +6,72 @@ use ark_grumpkin::{constraints::GVar as GVar2, Projective as Projective2};
 use ark_light_bitcoin_client::{
     gadgets::{
         block_hash_eq_broken_hash::{BlockHashCompareBrokenHash, ComputeBrokenHash},
+        block_header_hash_gadget::BlockHeaderHashGadget,
+        calculate_target_gadget::BlockTargetGadget,
         BTCBlockCheckerGadget,
     },
-    get_block_hash, get_target, read_blocks,
+    get_block_hash, get_block_hash_field_element, get_target, read_blocks,
     utils::{get_broken_hash, get_broken_hash_field_elements, Block, BlockVar},
 };
-use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar};
+use ark_r1cs_std::{
+    alloc::AllocVar, eq::EqGadget, fields::fp::FpVar, R1CSVar, ToBytesGadget,
+    ToConstraintFieldGadget,
+};
 use ark_relations::r1cs::{ConstraintSystemRef, SynthesisError};
 use ark_std::rand;
 use folding_schemes::{
     commitment::{kzg::KZG, pedersen::Pedersen},
-    folding::nova::{decider_eth::Decider, Nova},
+    folding::nova::{decider_eth::Decider as DeciderEth, Nova},
     frontend::FCircuit,
+    Error,
 };
 use folding_schemes::{folding::nova::decider_eth::prepare_calldata, Decider as DeciderTrait};
 use folding_schemes::{folding::nova::decider_eth_circuit::DeciderEthCircuit, FoldingScheme};
 use num_bigint::BigUint;
 use num_traits::Num;
 use solidity_verifiers::{
-    evm::save_solidity, get_decider_template_for_cyclefold_decider,
+    evm::Evm, g16::Groth16VerifierKey, kzg::KZG10VerifierKey, utils::get_formatted_calldata,
+    NovaCycleFoldVerifierKey,
+};
+use solidity_verifiers::{
+    evm::{compile_solidity, save_solidity},
+    get_decider_template_for_cyclefold_decider,
     utils::get_function_selector_for_nova_cyclefold_verifier,
 };
-use solidity_verifiers::{Groth16Data, KzgData, NovaCycleFoldData};
-use std::{fs, marker::PhantomData, time::Instant};
-use utils::setup;
+use std::{cmp::Ordering, fs, marker::PhantomData, time::Instant};
+
+use crate::utils::init_params;
 mod utils;
 
 #[derive(Clone, Debug)]
 pub struct BTCBlockCheckerFCircuit<F: PrimeField> {
     _f: PhantomData<F>,
-    block: Vec<Block>,
 }
 
 impl<F: PrimeField> FCircuit<F> for BTCBlockCheckerFCircuit<F> {
-    type Params = Vec<Block>;
+    type Params = ();
 
-    fn new(params: Self::Params) -> Self {
-        Self {
-            _f: PhantomData,
-            block: params,
-        }
+    fn new(params: Self::Params) -> Result<Self, Error> {
+        Ok(Self { _f: PhantomData })
     }
 
     fn state_len(&self) -> usize {
         3
     }
 
-    fn step_native(&self, i: usize, z_i: Vec<F>) -> Result<Vec<F>, folding_schemes::Error> {
-        // Check block hash
-        let computed_block_hash = get_block_hash(&self.block[i].block_header);
-        assert_eq!(computed_block_hash, self.block[i].block_hash);
-
-        // Check prev_block_hash
-        assert_eq!(
-            self.block[i].prev_block_hash,
-            hex::encode(&self.block[i].block_header[4..36].to_vec())
-        );
-
-        // Check pow
-        let target = get_target(&self.block[i].block_header);
-        let bigint_block_hash = BigUint::from_str_radix(&self.block[i].block_hash, 16).unwrap();
-        assert!(BigUint::from_bytes_be(&bigint_block_hash.to_bytes_le()) < target);
-
-        // Compute broken hash for previous block and compare to z_i
-        let prev_broken_hash = get_broken_hash(&self.block[i].prev_block_hash);
-        let broken_hash_field_elements = get_broken_hash_field_elements::<F>(prev_broken_hash);
-        assert_eq!(broken_hash_field_elements[0], z_i[1]);
-        assert_eq!(broken_hash_field_elements[1], z_i[2]);
-
-        // Compute broken hash for this block
-        let block_broken_hash = get_broken_hash(&self.block[i].block_hash);
-        let block_broken_hash_field_elements =
-            get_broken_hash_field_elements::<F>(block_broken_hash);
+    fn step_native(
+        &self,
+        i: usize,
+        z_i: Vec<F>,
+        external_inputs: Vec<F>,
+    ) -> Result<Vec<F>, folding_schemes::Error> {
+        // Compute block hash
+        let computed_block_hash = get_block_hash_field_element(&external_inputs);
 
         let new_z_i = vec![
             z_i[0] + F::one(),
-            block_broken_hash_field_elements[0],
-            block_broken_hash_field_elements[1],
+            computed_block_hash[0],
+            computed_block_hash[1],
         ];
 
         Ok(new_z_i)
@@ -91,39 +80,38 @@ impl<F: PrimeField> FCircuit<F> for BTCBlockCheckerFCircuit<F> {
     fn generate_step_constraints(
         &self,
         cs: ConstraintSystemRef<F>,
-        i: usize,
-        z_i: Vec<ark_r1cs_std::fields::fp::FpVar<F>>,
-    ) -> Result<Vec<ark_r1cs_std::fields::fp::FpVar<F>>, SynthesisError> {
-        // this is the block hash of the previous block
-        // it must be represented into 2 FpVars since we need to conform to the trait signature
-        let (block_hash_0, block_hash_1) = (z_i[1].clone(), z_i[2].clone());
-
-        let block_var = BlockVar::new_witness(cs.clone(), || Ok(self.block[i].clone()))?;
-        let _ = BTCBlockCheckerGadget::check_block(cs.clone(), block_var.clone())?;
-
-        // check that this fold's prev broken hash corresponds to the prev block hash
-        let _ = BlockHashCompareBrokenHash::compare_block_hash_to_broken_hash(
+        _i: usize,
+        z_i: Vec<FpVar<F>>,
+        external_inputs: Vec<FpVar<F>>,
+    ) -> Result<Vec<FpVar<F>>, SynthesisError> {
+        // check block (hash in header, compute block hash and check pow)
+        let computed_block_hash = BTCBlockCheckerGadget::check_block_fpvars(
             cs.clone(),
-            block_var.prev_block_hash,
-            vec![block_hash_0, block_hash_1],
-        );
+            vec![z_i[1].clone(), z_i[2].clone()],
+            external_inputs,
+        )?;
 
-        // output this block hash into a broken hash, which will be the next fold's prev block hash
-        let new_broken_hash =
-            ComputeBrokenHash::compute_broken_hash(cs.clone(), block_var.block_hash)?;
+        // output computed block hash as 2 FpVars
+        let computed_block_hash_broken =
+            ComputeBrokenHash::compute_broken_hash_from_digest(cs.clone(), computed_block_hash)?;
 
         let new_z_i = vec![
-            z_i[0].clone() + FpVar::new_constant(cs.clone(), F::one())?,
-            new_broken_hash[0].clone(),
-            new_broken_hash[1].clone(),
+            z_i[0].clone() + FpVar::<F>::Constant(F::one()),
+            computed_block_hash_broken[0].clone(),
+            computed_block_hash_broken[1].clone(),
         ];
+
         Ok(new_z_i)
+    }
+
+    fn external_inputs_len(&self) -> usize {
+        80
     }
 }
 
 fn main() {
     let file = include_str!("./data/btc-blocks.json");
-    let (mut prev_block_hash, blocks) = read_blocks(100, 1, file);
+    let (mut prev_block_hash, blocks) = read_blocks(3, 1, file);
 
     let mut blocks_prepared = vec![];
     for batch in blocks.iter() {
@@ -134,13 +122,14 @@ fn main() {
             serde_json::from_value::<Vec<Vec<u8>>>(batch.get("blockHeaders").unwrap().clone())
                 .unwrap();
         for (block_hash, block_header) in block_hashes.iter().zip(block_headers) {
+            let block_hash_str = block_hash.to_string();
             let block = Block {
                 block_header,
-                block_hash: block_hash.to_string(),
+                block_hash: block_hash_str.clone(),
                 prev_block_hash,
             };
             blocks_prepared.push(block.clone());
-            prev_block_hash = block_hash.to_string();
+            prev_block_hash = block_hash_str;
         }
     }
 
@@ -154,7 +143,7 @@ fn main() {
         Pedersen<Projective2>,
     >;
 
-    type DECIDER = Decider<
+    pub type DeciderEthFcircuit = DeciderEth<
         Projective,
         GVar,
         Projective2,
@@ -166,55 +155,48 @@ fn main() {
         NOVA,
     >;
 
-    let n_blocks_checked = blocks_prepared.len() / 20; // TMP rm '/20'
-    let circuit = BTCBlockCheckerFCircuit::<Fr>::new(blocks_prepared.clone());
-    let (fs_prover_params, kzg_vk) = setup(circuit.clone());
-    let z_0 = vec![Fr::from(0)];
-    let mut nova = NOVA::init(&fs_prover_params, circuit, z_0.clone()).unwrap();
+    let n_blocks_checked = blocks_prepared.len();
+    let circuit = BTCBlockCheckerFCircuit::<Fr>::new(()).unwrap();
+    let block_broken_hash = get_broken_hash(&blocks_prepared[0].prev_block_hash);
+    let block_broken_hash_field_elements = get_broken_hash_field_elements::<Fr>(block_broken_hash);
+
+    let z_0 = vec![
+        Fr::from(0),
+        block_broken_hash_field_elements[0],
+        block_broken_hash_field_elements[1],
+    ];
+
+    let (fs_prover_params, kzg_vk, g16_pk, g16_vk) =
+        init_params::<BTCBlockCheckerFCircuit<Fr>>(z_0.clone());
+
+    let mut nova = NOVA::init(&fs_prover_params, circuit.clone(), z_0.clone()).unwrap();
 
     println!("Computing folds...");
-    let start = Instant::now();
-    for i in 0..n_blocks_checked {
+    for (i, block) in blocks_prepared.iter().enumerate() {
         let current_state = nova.z_i[0].into_bigint();
         if i % 10 == 0 {
             println!("--- At block: {}/{} ---", current_state, n_blocks_checked);
         }
-        nova.prove_step().unwrap();
+        // header is 80 field elements
+        let mut header = vec![];
+        for h in block.block_header.clone().into_iter() {
+            header.push(Fr::from(h));
+        }
+
+        nova.prove_step(header).unwrap();
     }
-    println!(
-        "Done folding. Checked {} blocks in: {:.2?}",
-        n_blocks_checked,
-        start.elapsed()
-    );
 
-    let decider_circuit = DeciderEthCircuit::<
-        Projective,
-        GVar,
-        Projective2,
-        GVar2,
-        KZG<Bn254>,
-        Pedersen<Projective2>,
-    >::from_nova::<BTCBlockCheckerFCircuit<Fr>>(nova.clone())
+    let rng = rand::rngs::OsRng;
+    let start = Instant::now();
+    let proof = DeciderEthFcircuit::prove(
+        (g16_pk, fs_prover_params.cs_params.clone()),
+        rng,
+        nova.clone(),
+    )
     .unwrap();
-    let mut rng = rand::rngs::OsRng;
+    println!("generated Decider proof: {:?}", start.elapsed());
 
-    // decider setup
-    println!("Starting setup...");
-    let start = Instant::now();
-    let (g16_pk, g16_vk) =
-        Groth16::<Bn254>::circuit_specific_setup(decider_circuit.clone(), &mut rng).unwrap();
-    println!("Setup done in: {:.2?}", start.elapsed());
-
-    // decider proof generation
-    println!("Generating proof...");
-    let start = Instant::now();
-    let decider_pp = (g16_pk, fs_prover_params.cs_params.clone());
-    let proof = DECIDER::prove(decider_pp, rng, nova.clone()).unwrap();
-    println!("Proof generated in: {:.2?}", start.elapsed());
-
-    // decider proof verification
-    println!("Verifying proof...");
-    let verified = DECIDER::verify(
+    let verified = DeciderEthFcircuit::verify(
         (g16_vk.clone(), kzg_vk.clone()),
         nova.i,
         nova.z_0.clone(),
@@ -225,13 +207,9 @@ fn main() {
     )
     .unwrap();
     assert!(verified);
+    println!("Decider proof verification: {}", verified);
 
-    let g16_data = Groth16Data::from(g16_vk);
-    let kzg_data = KzgData::from((
-        kzg_vk,
-        fs_prover_params.cs_params.powers_of_g[0..3].to_vec(),
-    ));
-    let nova_cyclefold_data = NovaCycleFoldData::from((g16_data, kzg_data, nova.z_0.len()));
+    // Now, let's generate the Solidity code that verifies this Decider final proof
     let function_selector =
         get_function_selector_for_nova_cyclefold_verifier(nova.z_0.len() * 2 + 1);
 
@@ -246,113 +224,26 @@ fn main() {
     )
     .unwrap();
 
-    let decider_template = get_decider_template_for_cyclefold_decider(nova_cyclefold_data);
+    // prepare the setup params for the solidity verifier
+    let nova_cyclefold_vk = NovaCycleFoldVerifierKey::from((g16_vk, kzg_vk, circuit.state_len()));
 
-    // save smart contract
-    save_solidity("./NovaLightBTCClientDecider.sol", &decider_template.clone());
-    // save calldata
-    fs::write("./solidity-calldata.calldata", calldata.clone()).unwrap();
+    // generate the solidity code
+    let decider_solidity_code = get_decider_template_for_cyclefold_decider(nova_cyclefold_vk);
 
-    let nova_cyclefold_verifier_bytecode = compile_solidity(decider_template, "NovaDecider");
-
+    // verify the proof against the solidity code in the EVM
+    let nova_cyclefold_verifier_bytecode = compile_solidity(&decider_solidity_code, "NovaDecider");
     let mut evm = Evm::default();
     let verifier_address = evm.create(nova_cyclefold_verifier_bytecode);
-
-    let (_, output) = evm.call(verifier_address, calldata);
-    println!("Output: {:?}", output);
+    let (_, output) = evm.call(verifier_address, calldata.clone());
     assert_eq!(*output.last().unwrap(), 1);
-}
 
-#[cfg(test)]
-mod tests {
-    use std::fs;
-    use std::time::Instant;
-
-    use crate::{utils::setup, BTCBlockCheckerFCircuit};
-    use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as Projective};
-    use ark_ff::PrimeField;
-    use ark_grumpkin::{constraints::GVar as GVar2, Projective as Projective2};
-    use ark_light_bitcoin_client::read_blocks;
-    use ark_light_bitcoin_client::utils::{get_broken_hash, get_broken_hash_field_elements, Block};
-    use folding_schemes::FoldingScheme;
-    use folding_schemes::{
-        commitment::{kzg::KZG, pedersen::Pedersen},
-        folding::nova::Nova,
-        frontend::FCircuit,
-    };
-    use solidity_verifiers::utils::get_formatted_calldata;
-
-    #[test]
-    fn print_formatted_calldata() {
-        let calldata = fs::read("./solidity-calldata.calldata").unwrap();
-        let calldata_formatted = get_formatted_calldata(calldata);
-        for line in calldata_formatted {
-            println!("{}", line);
-        }
-    }
-
-    #[test]
-    fn run_folds() {
-        let file = include_str!("./data/btc-blocks.json");
-        let (mut prev_block_hash, blocks) = read_blocks(100, 1, file);
-
-        let mut blocks_prepared = vec![];
-        for batch in blocks.iter() {
-            let block_hashes =
-                serde_json::from_value::<Vec<String>>(batch.get("blockHashes").unwrap().clone())
-                    .unwrap();
-            let block_headers =
-                serde_json::from_value::<Vec<Vec<u8>>>(batch.get("blockHeaders").unwrap().clone())
-                    .unwrap();
-            for (block_hash, block_header) in block_hashes.iter().zip(block_headers) {
-                let block = Block {
-                    block_header,
-                    block_hash: block_hash.to_string(),
-                    prev_block_hash,
-                };
-                blocks_prepared.push(block.clone());
-                prev_block_hash = block_hash.to_string();
-            }
-        }
-
-        type NOVA = Nova<
-            Projective,
-            GVar,
-            Projective2,
-            GVar2,
-            BTCBlockCheckerFCircuit<Fr>,
-            KZG<'static, Bn254>,
-            Pedersen<Projective2>,
-        >;
-
-        let n_blocks_checked = blocks_prepared.len();
-        let circuit = BTCBlockCheckerFCircuit::<Fr>::new(blocks_prepared.clone());
-        let (prover_params, poseidon_config, kzg_vk) = setup(circuit.clone());
-
-        let block_broken_hash = get_broken_hash(&blocks_prepared[0].prev_block_hash);
-        let block_broken_hash_field_elements =
-            get_broken_hash_field_elements::<Fr>(block_broken_hash);
-
-        let z_0 = vec![
-            Fr::from(0),
-            block_broken_hash_field_elements[0],
-            block_broken_hash_field_elements[1],
-        ];
-        let mut nova = NOVA::init(&prover_params, circuit, z_0.clone()).unwrap();
-
-        println!("Computing folds...");
-        let now = Instant::now();
-        for i in 0..n_blocks_checked {
-            let current_state = nova.z_i[0].into_bigint();
-            if i % 10 == 0 {
-                println!("--- At block: {}/{} ---", current_state, n_blocks_checked);
-            }
-            nova.prove_step().unwrap();
-        }
-        let elapsed = now.elapsed();
-        println!(
-            "Done folding. Checked {} blocks in: {:.2?}",
-            n_blocks_checked, elapsed
-        );
-    }
+    // save smart contract and the calldata
+    fs::write(
+        "./BTCLightClientNovaVerifier.sol",
+        decider_solidity_code.clone(),
+    )
+    .unwrap();
+    fs::write("./solidity-calldata.calldata", calldata.clone()).unwrap();
+    let s = solidity_verifiers::utils::get_formatted_calldata(calldata.clone());
+    fs::write("./solidity-calldata.txt", s.join(",\n")).expect("");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,19 @@
+use std::time::Instant;
+
 use crate::BTCBlockCheckerFCircuit;
-use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as Projective};
-use ark_grumpkin::{constraints::GVar as GVar2, Projective as Projective2};
+use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
+use ark_crypto_primitives::snark::SNARK;
+use ark_groth16::Groth16;
+use ark_groth16::ProvingKey;
+use ark_groth16::VerifyingKey as G16VerifierKey;
+use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
+use ark_light_bitcoin_client::utils::Block;
 use ark_poly_commit::kzg10::VerifierKey as KZGVerifierKey;
+use ark_r1cs_std::fields::fp::FpVar;
+use ark_std::rand;
+use folding_schemes::folding::nova::decider_eth_circuit::DeciderEthCircuit;
+use folding_schemes::folding::nova::Nova;
+use folding_schemes::FoldingScheme;
 use folding_schemes::{
     commitment::{
         kzg::{ProverKey as KZGProverKey, KZG},
@@ -9,35 +21,59 @@ use folding_schemes::{
         CommitmentScheme,
     },
     folding::nova::{get_cs_params_len, ProverParams},
+    frontend::FCircuit,
     transcript::poseidon::poseidon_test_config,
 };
+use num_traits::Zero;
 
-pub fn setup(
-    circuit: BTCBlockCheckerFCircuit<Fr>,
-) -> (
-    ProverParams<Projective, Projective2, KZG<'static, Bn254>, Pedersen<Projective2>>,
+pub fn init_test_prover_params<FC: FCircuit<Fr, Params = ()>>() -> (
+    ProverParams<G1, G2, KZG<'static, Bn254>, Pedersen<G2>>,
     KZGVerifierKey<Bn254>,
 ) {
     let mut rng = ark_std::test_rng();
     let poseidon_config = poseidon_test_config::<Fr>();
-
+    let f_circuit = FC::new(()).unwrap();
     let (cs_len, cf_cs_len) =
-        get_cs_params_len::<Projective, GVar, Projective2, GVar2, BTCBlockCheckerFCircuit<Fr>>(
-            &poseidon_config,
-            circuit,
+        get_cs_params_len::<G1, GVar, G2, GVar2, FC>(&poseidon_config, f_circuit).unwrap();
+    let (kzg_pk, kzg_vk): (KZGProverKey<G1>, KZGVerifierKey<Bn254>) =
+        KZG::<Bn254>::setup(&mut rng, cs_len).unwrap();
+    let (cf_pedersen_params, _) = Pedersen::<G2>::setup(&mut rng, cf_cs_len).unwrap();
+    let fs_prover_params = ProverParams::<G1, G2, KZG<Bn254>, Pedersen<G2>> {
+        poseidon_config: poseidon_config.clone(),
+        cs_params: kzg_pk.clone(),
+        cf_cs_params: cf_pedersen_params,
+    };
+    (fs_prover_params, kzg_vk)
+}
+
+pub fn init_params<FC: FCircuit<Fr, Params = ()>>(
+    z_0: Vec<Fr>,
+) -> (
+    ProverParams<G1, G2, KZG<'static, Bn254>, Pedersen<G2>>,
+    KZGVerifierKey<Bn254>,
+    ProvingKey<Bn254>,
+    G16VerifierKey<Bn254>,
+) {
+    let mut rng = rand::rngs::OsRng;
+    let start = Instant::now();
+    let (fs_prover_params, kzg_vk) = init_test_prover_params::<FC>();
+    println!("generated Nova folding params: {:?}", start.elapsed());
+    let f_circuit = FC::new(()).unwrap();
+
+    pub type NOVA<FC> = Nova<G1, GVar, G2, GVar2, FC, KZG<'static, Bn254>, Pedersen<G2>>;
+    let nova = NOVA::init(&fs_prover_params, f_circuit, z_0.clone()).unwrap();
+
+    let decider_circuit =
+        DeciderEthCircuit::<G1, GVar, G2, GVar2, KZG<Bn254>, Pedersen<G2>>::from_nova::<FC>(
+            nova.clone(),
         )
         .unwrap();
-
-    let (kzg_pk, kzg_vk): (KZGProverKey<Projective>, KZGVerifierKey<Bn254>) =
-        KZG::<Bn254>::setup(&mut rng, cs_len).unwrap();
-    let (cf_pedersen_params, _) = Pedersen::<Projective2>::setup(&mut rng, cf_cs_len).unwrap();
-
-    (
-        ProverParams::<Projective, Projective2, KZG<Bn254>, Pedersen<Projective2>> {
-            poseidon_config: poseidon_config.clone(),
-            cs_params: kzg_pk.clone(),
-            cf_cs_params: cf_pedersen_params,
-        },
-        kzg_vk,
-    )
+    let start = Instant::now();
+    let (g16_pk, g16_vk) =
+        Groth16::<Bn254>::circuit_specific_setup(decider_circuit.clone(), &mut rng).unwrap();
+    println!(
+        "generated G16 (Decider circuit) params: {:?}",
+        start.elapsed()
+    );
+    (fs_prover_params, kzg_vk, g16_pk, g16_vk)
 }
