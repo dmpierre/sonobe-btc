0d96b68d4e55afab5d50d1a87420e774b5e8d616
dmpierre <pdaixmoreux@gmail.com>
2024-05-20 12:05:13 +0200

    chore: update .sh

--- setup-machine.sh
+++ setup-machine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# running on m7i.metal-24xl instance
-# run with `source ./setup-machine.sh`
 sudo apt-get update
 sudo apt install build-essential clang tmux -y
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
-git clone https://github.com/dmpierre/folding-schemes-light-btc.git
+# add solc bin in /usr/local/bin (!bin is for linux x64 arch!)
+wget https://github.com/ethereum/solidity/releases/download/v0.8.25/solc-static-linux
+sudo mv solc-static-linux /usr/local/bin/solc
