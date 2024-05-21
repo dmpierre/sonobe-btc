0f19c85d4e60657a8743717e044fdb925d4d8510
dmpierre <pdaixmoreux@gmail.com>
2024-05-21 12:18:22 +0200

    feat: make contract ownable and add remappings

--- contracts/src/BTCLightClient.sol
+++ contracts/src/BTCLightClient.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 import {NovaDecider} from "./BTCLightClientNovaVerifier.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BTCLightClient {
+contract BTCLightClient is Ownable {
 
     uint256 public blocksVerified; // Number of blocks verified
     uint256[2] public startBlock; // First verified pow block
@@ -10,14 +11,13 @@ contract BTCLightClient {
 
     NovaDecider decider;
 
-    constructor(address deciderAdress) {
+    constructor(address deciderAdress) Ownable(msg.sender) {
         decider = NovaDecider(deciderAdress);
     }
 
     function setDecider(address deciderAdress) public {
         decider = NovaDecider(deciderAdress);
     }
-
 
     function setBlocksVerified( 
         uint256[7] calldata i_z0_zi,
@@ -30,7 +30,7 @@ contract BTCLightClient {
         uint256[2] calldata pC,
         uint256[4] calldata challenge_W_challenge_E_kzg_evals,
         uint256[2][2] calldata kzg_proof
-) public {
+    ) public onlyOwner {
         require(decider.verifyNovaProof(i_z0_zi, U_i_cmW_U_i_cmE, U_i_u_u_i_u_r, U_i_x_u_i_cmW, u_i_x_cmT, pA, pB, pC, challenge_W_challenge_E_kzg_evals, kzg_proof), "Proof: verification failed");
         if (blocksVerified > 0) {
             // we require to start from the last verified block
